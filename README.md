6c32a231f0b05043c8893c57e1c7b4f5594cde96
lesujuca <lesujuca@gmail.com>
2024-10-18 21:10:13 -0400

    Subida del proyecto primera entrega

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# entrega1Unet
+# 1ra Entrega Curso Full Stack UNET
 Ensayo 1ra entrega curso full stck UNET
+
+Mi entrega es sobre un mini portfolio personal.
+
+La página es de tipo Single Page Application (SPA), es decir todo el contenido dinámico se actualiza y carga en una misma página.
+
+Se realizó una maquetación con colores claros principalmente con una modalidad de contraste para modo oscuro.
+
+En la cabecera cargo una imagen de perfil (cabe destacar que es una foto de mi hija), igualmente en la parte superior derecha tenemos un link para cambiar de modo claro a oscuro y viceversa. Tenemos un título principal y secundario, seguido más abajo de un menú principal.
+
+En el cuerpo principal, se tiene una sección que se compone de tres pequeñas partes:
+    1.- Sobre Mi: Esta sección tiene una breve descripción de mi nivel académico.
+    2.- Habilidades: En esta sección enumero los diferentes lenguajes y tecnologías que manejo actualmente.
+    3.- Proyectos: En esta sección se muestran una breve descripción de los proyectos en los que he colaborado en mi parte profesional y cada uno separado en tarjetas de presentación.
+
+Como parte del cuerpo secundario se encuentra la sección aside (barra lateral) donde tiene dos secciones:
+    1- Muestra el clima de San Cristóbal consumiendo la api de open-meteo de forma gratuita y cada diez(10) minutos refresca los valores.
+    2- Carga una lista de 10 noticias de los departamentos de tecnología, computación, informática y hardware. Esta se refresca cada diez(10) minutos.
+
+En la parte inferior se encuentra la sección de footer donde muestro los derechos de autor, un enlace para abrir el modal de un twitter en concreto de la plataforma "X", datos de contacto como email y teléfono para finalizar con una breve explicación del compromiso que se tiene con el acceso a la página y un correo de atención.
+
+La página está diseñada para ser resposive con unos colores pastel y alternando en unos colores más oscuros manteniendo el mismo aspecto.
+
+Espero sea de su agrado, saludos!!!
---
+++ js/script.js
@@ -0,0 +1,149 @@
+// Variables el id de los elementos modales
+var modalTwitter = document.getElementById("modalTwitter");
+var modalContact = document.getElementById("modalContact");
+var modalUbicacion = document.getElementById("modalUbicacion");
+
+// Variables el id de los enlaces
+var btnTwitter = document.getElementById("openModalTwitter");
+var btnContact = document.getElementById("openModalContact");
+var btnUbicacion = document.getElementById("openModalUbicacion");
+
+// Variables el id de los elementos para cerrar los modales
+var spanTwitter = document.getElementsByClassName("close")[0];
+var spanContact = document.getElementsByClassName("close")[1];
+var spanUbicacion = document.getElementsByClassName("close")[2];
+
+// función al dar click para mostrar el modal de X
+btnTwitter.onclick = function() {
+    modalTwitter.style.display = "block";
+}
+
+// función al dar click para mostrar el formulario de Contacto
+btnContact.onclick = function() {
+    modalContact.style.display = "block";
+}
+
+// función al dar click para mostrar el modal de Ubicación
+btnUbicacion.onclick = function() {
+    modalUbicacion.style.display = "block";
+}
+
+// función al dar click para cerrar el modal de X
+spanTwitter.onclick = function() {
+    modalTwitter.style.display = "none";
+}
+
+// función al dar click para cerrar el modal de Contacto
+spanContact.onclick = function() {
+    modalContact.style.display = "none";
+}
+
+// función al dar click para cerrar el modal de Ubicación
+spanUbicacion.onclick = function() {
+    modalUbicacion.style.display = "none";
+}
+
+// función al dar click fuera del modal
+window.onclick = function(event) {
+    if (event.target == modalTwitter) {
+        modalTwitter.style.display = "none";
+    }
+
+    if (event.target == modalContact) {
+        modalContact.style.display = "none";
+    }
+
+    if (event.target == modalUbicacion) {
+        modalUbicacion.style.display = "none";
+    }
+}
+
+// función para expandir la sección de sobre mi
+document.getElementById('expandAbout').addEventListener('click', function(event) {
+    event.preventDefault();
+    var details = document.getElementById('about');
+    if (!details.open) {
+        details.querySelector('summary').click();
+    }
+});
+
+// función para expandir la sección de habilidades
+document.getElementById('expandHabilidades').addEventListener('click', function(event) {
+    event.preventDefault();
+    var details = document.getElementById('skills');
+    if (!details.open) {
+        details.querySelector('summary').click();
+    }
+});
+
+// función para consumir la api de noticias y mostrarla en el div de noticias
+async function fetchNews() {
+    const apiKey = '93dd4505683f42d790f06de23580077b';
+    const url = `https://newsapi.org/v2/everything?q=informatics OR computation OR tecnology OR hardware&language=es&pageSize=10&apiKey=${apiKey}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const articles = data.articles;
+        const newsList = document.getElementById('news-list');
+        newsList.innerHTML = ''; // Limpia la lista antes de agregar nuevas noticias
+        articles.forEach(article => {
+            const listItem = document.createElement('li');
+            const link = document.createElement('a');
+            link.href = article.url;
+            link.textContent = article.title;
+            link.target = '_blank'; // Abre el enlace en una nueva pestaña
+            listItem.appendChild(link);
+            newsList.appendChild(listItem);
+        });
+    } catch (error) {
+        console.error('Error fetching news:', error);
+    }
+}
+
+// función para consumir la api de clima y mostrarla en el div de clima
+async function clima() {
+    fetch('https://api.open-meteo.com/v1/forecast?latitude=7.8101945&longitude=-72.1988272&current_weather=true&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m')
+        .then(response => response.json())
+        .then(data => {
+            const weatherDiv = document.getElementById('weather');
+            const temperature = data.current_weather.temperature;
+            const windSpeed = data.current_weather.windspeed;
+
+            weatherDiv.innerHTML = `
+                <p>Temperatura: ${temperature}°C</p>
+                <p>Velocidad del viento: ${windSpeed} km/h</p>
+            `;
+        })
+        .catch(error => {
+            const weatherDiv = document.getElementById('weather');
+            weatherDiv.innerHTML = `<p>Error al cargar datos del clima.</p>`;
+            console.error('Error:', error);
+        });
+}
+
+// función para cambiar los colores según el tema
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('toggle-mode');
+    const body = document.body;
+    
+    // Verificar si el usuario ya tiene una preferencia guardada
+    if (localStorage.getItem('theme') === 'dark') {
+        body.classList.add('dark-mode');
+        toggleButton.textContent = 'Cambiar a Modo Claro';
+    } else {
+        body.classList.remove('dark-mode');
+        toggleButton.textContent = 'Cambiar a Modo Oscuro';
+    }
+  
+    toggleButton.addEventListener('click', function() {
+        body.classList.toggle('dark-mode');
+
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('theme', 'dark');
+            toggleButton.textContent = 'Cambiar a Modo Claro';
+        } else {
+            localStorage.setItem('theme', 'light');
+            toggleButton.textContent = 'Cambiar a Modo Oscuro';
+        }
+    });
+});
